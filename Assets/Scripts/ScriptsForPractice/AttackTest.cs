cab80466452eaa77c89bf90514efe665427f9b74
milquejas <84683894+milquejas@users.noreply.github.com>
2023-02-02 11:12:02 +0200

    ..
    ..

--- Assets/Scripts/ScriptsForPractice/AttackTest.cs
+++ Assets/Scripts/ScriptsForPractice/AttackTest.cs
@@ -7,7 +7,7 @@ public class AttackTest : MonoBehaviour
     public float jumpHeight;
     public float speed;
     public Rigidbody2D rb2D;
-    public GameObject player;
+    
 
     public LayerMask playerLayer;
     public LayerMask groundLayer;
@@ -20,53 +20,63 @@ public class AttackTest : MonoBehaviour
     private bool isFacingRight;
     public bool isGrounded = true;
     public bool isWalking = true;
+    
 
-    public bool playerOnRange = true;
-
+    public Transform player;
+    public float chaseSpeed = 8f;
+    private float minDepth;
+    private float maxDepth;
 
     void Start()
     {
         isFacingRight = true;
         rb2D = GetComponent<Rigidbody2D>();
-        
-    }
-
-    void Patrol()
-    {
-        // move the enemy in the direction it is facing
-        rb2D.velocity = new Vector2(speed * (isFacingRight ? 1 : -1), rb2D.velocity.y);
-    }
-
-    private void FollowPlayer()
-    {
-    //    if(playerOnRange)
-    //    {
-    //        ////tarkastaa suunnan
-            Vector2 direction = isFacingRight ? Vector2.right : Vector2.left;
-
-    //        tarkastaa playerLayerin
-            RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position,
-               direction, raycastPlayerDistance, playerLayer);
-
-    //        // Calculate the distance between the enemy and the player
-    //        float distance = Vector2.Distance(transform.position, player.transform.position);
-
-    //        // Get the horizontal direction towards the player
-    //        float xDirection = (player.transform.position.x > transform.position.x) ? 1 : -1;
-
-            //Follow the player
-            rb2D.velocity = new Vector2(speed * (player.transform.position.x - transform.position.x), rb2D.velocity.y);
-
-    //    }
 
     }
-
-
     void Flip()
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0, 180, 0);
     }
+    void Patrol()
+    {
+        // move the enemy in the direction it is facing
+        rb2D.velocity = new Vector2(speed * (isFacingRight ? 1 : -1), rb2D.velocity.y);
+    }
+    
+    public void Chase()
+    {
+        //alkaa jahtaamaan pelaajaa kun raycast osuu pelaajaan
+        RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position,
+        isFacingRight ? Vector2.right : Vector2.left, raycastPlayerDistance, playerLayer);
+             
+        //Follow the player
+        rb2D.velocity = new Vector2(chaseSpeed * (player.transform.position.x - transform.position.x), rb2D.velocity.y);
+
+    }
+    //private void FollowPlayer()
+    //{
+    ////    if(playerOnRange)
+    ////    {
+    ////        ////tarkastaa suunnan
+    //        Vector2 direction = isFacingRight ? Vector2.right : Vector2.left;
+
+    ////        tarkastaa playerLayerin
+    //        RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position,
+    //           direction, raycastPlayerDistance, playerLayer);
+
+    ////        // Calculate the distance between the enemy and the player
+    ////        float distance = Vector2.Distance(transform.position, player.transform.position);
+
+    ////        // Get the horizontal direction towards the player
+    ////        float xDirection = (player.transform.position.x > transform.position.x) ? 1 : -1;
+
+    //        //Follow the player
+    //        rb2D.velocity = new Vector2(speed * (player.transform.position.x - transform.position.x), rb2D.velocity.y);
+
+    ////    }
+
+    //}
 
     private void Update()
     {
@@ -83,14 +93,10 @@ public class AttackTest : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(transform.position,
             direction, raycastWallDistance, wallLayer);
 
-        
-        
+        //tarkastaa playerLayerin
+        RaycastHit2D hitPlayer = Physics2D.Raycast(transform.position,
+              direction, playerLayer);
 
-
-        if (isGrounded)
-        {
-            Patrol();
-        }
 
         // if the raycast hits a wall
         if (hit.collider != null)
@@ -98,14 +104,41 @@ public class AttackTest : MonoBehaviour
             // make the enemy turn around
             Flip();
         }
-        if (playerOnRange)
+
+        if (isGrounded)
         {
-            FollowPlayer();
-            if (!playerOnRange)
-            {
-                Patrol();
-            }
+            Patrol();
         }
+
+        if (hitPlayer.collider != null)
+        {
+            Chase();
+        }
+
+        //float distance = Vector2.Distance(transform.position, player.position);
+        //if (distance < followRange)
+        //{
+        //    float xDirection = (player.transform.position.x > transform.position.x) ? 1 : -1;
+        //    transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime * xDirection);
+        //}
+        //tarkastaa enemyn ja pelaajan välisen etäisyyden
+        //float distance = Vector2.Distance(transform.position, player.position);
+        //float xDirection = (player.transform.position.x > transform.position.x) ? 1 : -1;
+
+        //if (distance < followRange)
+        //{
+        //    transform.position = Vector2.MoveTowards
+        //        (transform.position, player.position,xDirection, speed * Time.deltaTime);
+
+        //}
+        //if (playerOnRange)
+        //{
+        //    FollowPlayer();
+        //    if (!playerOnRange)
+        //    {
+        //        Patrol();
+        //    }
+        //}
     }
     
 }
