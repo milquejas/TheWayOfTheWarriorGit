ad7a0362355b7ea98d4ae4a6f6af8a755ed9f411
milquejas <84683894+milquejas@users.noreply.github.com>
2023-02-07 07:56:57 +0200

    ..
    sasdsads

--- Assets/Scripts/ScriptsForPractice/UltimateEnemyAI.cs
+++ Assets/Scripts/ScriptsForPractice/UltimateEnemyAI.cs
@@ -24,14 +24,13 @@ public class UltimateEnemyAI : MonoBehaviour
 
     private bool isChase = false;
     private bool isSearching = false;
-    private bool isFacingRight = true;
+    public bool isFacingRight;
     public bool isGrounded = true;
     public bool isWalking = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        isFacingRight = true;
         rb2D = GetComponent<Rigidbody2D>();
     }
 
@@ -39,7 +38,7 @@ public class UltimateEnemyAI : MonoBehaviour
     {
         isChase = false;
         isSearching = false;
-        
+
 
         ////tarkastaa suunnan
         Vector2 direction = isFacingRight ? Vector2.right : Vector2.left;
@@ -77,14 +76,14 @@ public class UltimateEnemyAI : MonoBehaviour
     bool CanSeePlayer(float distance)
     {
         bool val = false;
-        float castDist = distance;
+        float castDist = -distance;
 
         //jos katsoo oikealle
-        if (isFacingRight)
-        {
+        //if (isFacingRight)
+        //{
 
-            castDist = -distance;
-        }
+        //    castDist = -distance;
+        //}
 
         //tarkastaa playerLayerin
         //RaycastHit2D hitPlayer = Physics2D.Raycast(eyeCast.transform.position, player.transform.position, playerLayer);
@@ -131,44 +130,44 @@ public class UltimateEnemyAI : MonoBehaviour
         //Animator.Play("")
     }
 
-    void Search()
-    {
-        // set isChase to false and isSearching to true
-        isChase = false;
-        isSearching = true;
+    //void Search()
+    //{
+    //    // set isChase to false and isSearching to true
+    //    isChase = false;
+    //    isSearching = true;
 
-        // play a searching animation
-        //Animator.Play("")
+    //    // play a searching animation
+    //    //Animator.Play("")
 
-        // start a timer to keep track of how long the enemy has been searching
-        float searchTimer = 0;
+    //    // start a timer to keep track of how long the enemy has been searching
+    //    float searchTimer = 0;
 
-        // while the enemy is searching
-        while (isSearching)
-        {
-            // update the search timer
-            searchTimer += Time.deltaTime;
+    //    // while the enemy is searching
+    //    while (isSearching)
+    //    {
+    //        // update the search timer
+    //        searchTimer += Time.deltaTime;
 
-            // check if the enemy can see the player
-            bool canSeePlayer = CanSeePlayer(chaseRange);
+    //        // check if the enemy can see the player
+    //        bool canSeePlayer = CanSeePlayer(chaseRange);
 
-            // if the enemy can see the player, switch to chase state
-            if (canSeePlayer)
-            {
-                isChase = true;
-                isSearching = false;
-                break;
-            }
+    //        // if the enemy can see the player, switch to chase state
+    //        if (canSeePlayer)
+    //        {
+    //            isChase = true;
+    //            isSearching = false;
+    //            break;
+    //        }
 
-            // if the search timer reaches a certain amount of time (e.g. 5 seconds)
-            if (searchTimer >= 5f)
-            {
-                // stop searching and go back to patrol state
-                isSearching = false;
-                break;
-            }
-        }
-    }
+    //        // if the search timer reaches a certain amount of time (e.g. 5 seconds)
+    //        if (searchTimer >= 5f)
+    //        {
+    //            // stop searching and go back to patrol state
+    //            isSearching = false;
+    //            break;
+    //        }
+    //    }
+    //}
 
 
 
@@ -180,25 +179,25 @@ public class UltimateEnemyAI : MonoBehaviour
 
     }
 
-    void FlipTowards()
-    {
-        // flip funktio
-        isFacingRight = !isFacingRight;
-        transform.Rotate(0, 180, 0);
+    //void FlipTowards()
+    //{
+    //    // flip funktio
+    //    isFacingRight = !isFacingRight;
+    //    transform.Rotate(0, 180, 0);
 
-        if (transform.position.x < player.transform.position.x)
-        {
-            //jos enemy on pelaajan vasemmalla puolella, liiku oikealle
-            transform.localScale = new Vector2(1, 1);
-        }
-        else
-        {
-            //toisin päin, liikkuu vasemmalle
-            transform.localScale = new Vector2(-1, 1);
-        }
+    //    if (transform.position.x < player.transform.position.x)
+    //    {
+    //        //jos enemy on pelaajan vasemmalla puolella, liiku oikealle
+    //        transform.localScale = new Vector2(1, 1);
+    //    }
+    //    else
+    //    {
+    //        //toisin päin, liikkuu vasemmalle
+    //        transform.localScale = new Vector2(-1, 1);
+    //    }
 
 
-    }
+    //}
 
 
     // Update is called once per frame
@@ -244,6 +243,7 @@ public class UltimateEnemyAI : MonoBehaviour
         }
         else
         {
+            
             // Stop funktio chase ja aloittaa patrol
             Patrol();
         }
